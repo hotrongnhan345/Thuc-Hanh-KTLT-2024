9705c557e1d55ecb862646499760721ade69a442
hotrongnhan345 <htnhan212005@gmail.com>
2024-12-11 13:44:05 +0700

    Add files via upload

---
+++ 3.1.py
@@ -0,0 +1,9 @@
+print("Sinh Viên: Hồ Trọng Nhân")
+print("MSSV: 235752021610123")
+def sum(a, b):
+    sum = a + b
+    print("Tổng của", a, "và", b, "là:", sum)
+
+# Ví dụ:
+sum(2, 3)
+sum(4, 5)
---
+++ 3.4.py
@@ -0,0 +1,8 @@
+print("Sinh Viên: Hồ Trọng Nhân")
+print("MSSV: 235752021610123")
+a = "Hello Guy!"
+def say(a):
+    a ="Vinh University"
+    print(a)
+say(a)
+print(a)
