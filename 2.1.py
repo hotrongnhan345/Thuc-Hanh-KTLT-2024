b3439a8bbe54531f5d3d4c27807206e7e49ac525
hotrongnhan345 <htnhan212005@gmail.com>
2024-12-11 13:36:14 +0700

    Add files via upload

---
+++ 2.1.py
@@ -0,0 +1,9 @@
+print("Sinh Viên: Hồ Trọng Nhân")
+print("MSSV: 235752021610123")
+n1 = int(input("Enter n1 value: "))
+n2 = int(input("Enter n2 value: "))
+
+if n1 > n2:
+    print("n1 is bigger")
+else:
+    print("n2 is bigger")
