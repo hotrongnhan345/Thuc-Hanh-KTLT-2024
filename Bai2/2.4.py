56a7790535bcee39c5a67f1e7efd08e8329f34af
hotrongnhan345 <htnhan212005@gmail.com>
2024-12-11 13:42:17 +0700

    Add files via upload

---
+++ Bai2/2.4.py
@@ -0,0 +1,6 @@
+print("Sinh Viên: Hồ Trọng Nhân")
+print("MSSV: 235752021610123")
+def nghich_dao(a, b):
+    for i in range (a, b+1):
+        for j in range (2, 11):
+            print(f"{i}/{j} = {i/j:.2f}")
