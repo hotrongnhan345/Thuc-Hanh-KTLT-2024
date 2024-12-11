e65ec3fa07d211bc6979581de4e56bfcfe48f95c
hotrongnhan345 <htnhan212005@gmail.com>
2024-12-11 14:16:55 +0700

    Add files via upload

---
+++ Bài6/bai5.1.py
@@ -0,0 +1,14 @@
+print("Sinh Vien: Ho Trong Nhan MSSV: 235752021610123")
+# Import module mymath
+import mymath
+
+# Sử dụng các hàm trong module
+a = 6
+b = 8
+
+print(f"{a} + {b} = {mymath.add(a, b)}")
+print(f"{a} - {b} = {mymath.subtract(a, b)}")
+print(f"{a} * {b} = {mymath.multiply(a, b)}")
+print(f"{a} / {b} = {mymath.divide(a, b)}")
+print(f"{a} ^ {b} = {mymath.power(a, b)}")
+
