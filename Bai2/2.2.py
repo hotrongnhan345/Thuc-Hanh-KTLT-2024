d08f61627620523196d6e54dd354f4271b4a035f
hotrongnhan345 <htnhan212005@gmail.com>
2024-12-11 13:40:54 +0700

    Add files via upload

---
+++ Bai2/2.2.py
@@ -0,0 +1,12 @@
+print("Sinh Viên: Hồ Trọng Nhân")
+print("MSSV: 235752021610123")
+import math
+
+x1 = int(input("Enter x1: "))
+y1 = int(input("Enter y1: "))
+x2 = int(input("Enter x2: "))
+y2 = int(input("Enter y2: "))
+
+distance = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+
+print("Distance:", distance)
